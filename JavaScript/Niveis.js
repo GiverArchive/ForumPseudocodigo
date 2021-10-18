8d11c98e9a18728dcb7af6966ed805213b3729a3
GiverPlay007 <play.giver.007@gmail.com>
2021-10-17 22:39:33 -0300

    Em português

--- JavaScript/Niveis.js
+++ JavaScript/Niveis.js
@@ -3,37 +3,37 @@
 
 const ULTIMO_NIVEL = 100;
 
-function Player(apelido, nivel, pontos) {
+function Jogador(apelido, nivel, pontos) {
   this.apelido = apelido;
   this.nivel = nivel;
   this.pontos = pontos;
 }
 
-function passarDeNivel(player) {
-  const { nivel, pontos } = player;
+function passarDeNivel(jogador) {
+  const { nivel, pontos } = jogador;
 
   if(nivel === ULTIMO_NIVEL) {
     return false;
   }
 
   if(pontos >= 1000) {
-    player.nivel++;
-    player.pontos = 0;
+    jogador.nivel++;
+    jogador.pontos = 0;
     return true;
   }
 
   return false;
 }
 
-function testarNivel(player) {
-  console.log(`${player.apelido} ${passarDeNivel(player) ? "passou" : "não passou"} de nível.`);
+function testarNivel(jogador) {
+  console.log(`${jogador.apelido} ${passarDeNivel(jogador) ? "passou" : "não passou"} de nível.`);
 }
 
-const mario = new Player("Mario", 10, 250);
-const jose = new Player("José", 100, 810);
-const ana = new Player("Ana", 87, 1000);
-const will = new Player("Will", 75, 1024);
-const giver = new Player("Giver", 100, 1000);
+const mario = new Jogador("Mario", 10, 250);
+const jose = new Jogador("José", 100, 810);
+const ana = new Jogador("Ana", 87, 1000);
+const will = new Jogador("Will", 75, 1024);
+const giver = new Jogador("Giver", 100, 1000);
 
 testarNivel(mario);
 testarNivel(jose);
