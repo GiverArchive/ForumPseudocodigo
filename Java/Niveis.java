75b796dc0fb980b9222c70e669ecac513247b0bd
GiverPlay007 <37253454+GiverPlay007@users.noreply.github.com>
2021-10-18 00:34:55 -0300

    Update Niveis.java
    Ordem alfabética

--- Java/Niveis.java
+++ Java/Niveis.java
@@ -6,39 +6,40 @@ public class Niveis {
   public static final int ULTIMO_NIVEL = 100;
 
   public static void main(String[] args) {
-    Jogador reds = new Jogador("Reds", 100, 150);
+
     Jogador akira = new Jogador("Akira", 89, 1000);
-    Jogador snow = new Jogador("Snow", 100, 1000);
-    Jogador neraid = new Jogador("Neraid", 74, 980);
+    Jogador andrade = new Jogador("Andrade", 93, 1000);
+    Jogador avesso = new Jogador("Avesso", 78, 1000);
     Jogador chep = new Jogador("Chep", 45, 240);
+    Jogador defmc = new Jogador("Defmc", 100, 980);
     Jogador dio = new Jogador("Dio", 72, 1000);
     Jogador felps = new Jogador("Felps", 49, 810);
-    Jogador jayesi = new Jogador("Jayesi", 64, 512);
-    Jogador gld = new Jogador("GLD", 100, 450);
     Jogador gabriel = new Jogador("Gabriel", 84, 869);
-    Jogador sonico = new Jogador("Sonicio", 100, 1000);
-    Jogador avesso = new Jogador("Avesso", 78, 1000);
-    Jogador migke = new Jogador("Mig", 68, 710);
-    Jogador defmc = new Jogador("Defmc", 100, 980);
     Jogador giovany = new Jogador("Giovany", 47, 1000);
-    Jogador andrade = new Jogador("andrade", 93, 1000);
-  
-    testarNivel(reds);
+    Jogador gld = new Jogador("GLD", 100, 450);
+    Jogador jayesi = new Jogador("Jayesi", 64, 512);  
+    Jogador migke = new Jogador("Mig", 68, 710);
+    Jogador neraid = new Jogador("Neraid", 74, 980);
+    Jogador reds = new Jogador("Reds", 100, 150);
+    Jogador snow = new Jogador("Snow", 100, 1000);
+    Jogador sonico = new Jogador("Sonicio", 100, 1000);
+
     testarNivel(akira);
-    testarNivel(snow);
-    testarNivel(neraid);
+    testarNivel(andrade);
+    testarNivel(avesso);
     testarNivel(chep);
+    testarNivel(defmc);
     testarNivel(dio);
     testarNivel(felps);
-    testarNivel(jayesi);
-    testarNivel(gld);
     testarNivel(gabriel);
-    testarNivel(sonico);
-    testarNivel(avesso);
-    testarNivel(migke);
-    testarNivel(defmc);
     testarNivel(giovany);
-    testarNivel(andrade);
+    testarNivel(gld);
+    testarNivel(jayesi);
+    testarNivel(migke);
+    testarNivel(neraid);
+    testarNivel(reds);
+    testarNivel(snow);
+    testarNivel(sonico);
   }
 
   public static void testarNivel(Jogador jogador) {
